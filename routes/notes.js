b378fb0054c229d29d06845d13e33af33b12aaec
Usman6982 <falakudaas4051@gmail.com>
2023-11-05 12:51:51 +0500

    CRUD start

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,45 @@
-const express = require ('express');
-// eslint-disable-next-line
-const { model } = require('mongoose');
+const express = require('express');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    
-    res.json([])
-     
-})
-module.exports = router
+// Route 1: Get All the notes using: GET "/api/auth/getuser". Login required
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
+// Route 2: Add a new Note using: POST "/api/auth/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 5 }),
+    body('description', 'Description must be at least 5 characters').isLength({ min: 5 }),
+    body('tag', 'Enter a valid tag').isLength({ min: 3 }),
+], async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        // If there are errors, return bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title,
+            description,
+            tag,
+            user: req.user.id,
+        });
+        const savedNote = await note.save();
+        res.json(savedNote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
+module.exports = router;
